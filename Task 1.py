a882cb8f6b8021e43647aad355e4ffe803e1cd6b
Raghavk01 <Raghavkapoor1211@gmail.com>
2025-06-01 00:04:40 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+Addition=num1+num2
+print("Addition:",Addition)
+subtaction=num1-num2
+print("subtaction:",subtaction)
+multiply=num1*num2
+print("multiply:",multiply)
+Division=num1/num2
+print("Division:",Division)
---
+++ Task 2.py
@@ -0,0 +1,4 @@
+Firstname=input("Enter the First name: ")
+Secondname=input("Enter the Second name: ")
+fullname=Firstname+" "+Secondname
+print("Hello, "+fullname+"! welcome to python program session.raghav ")
